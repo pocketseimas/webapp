e7d73fb8139f1ca9f1e9eebbfcdfbcea11a44935
lizz <home@lizz.dev>
2025-02-19 22:50:07 +0200

    Initial commit

---
+++ src/main/kotlin/com/pocketseimas/pocketseimas/PocketSeimasApplication.kt
@@ -0,0 +1,11 @@
+package com.pocketseimas.pocketseimas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocketSeimasApplication
+
+fun main(args: Array<String>) {
+    runApplication<PocketSeimasApplication>(*args)
+}
---
+++ src/test/kotlin/com/pocketseimas/pocketseimas/PocketSeimasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pocketseimas.pocketseimas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocketSeimasApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
